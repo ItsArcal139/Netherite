b14a9c6fe5eeb5d9274161fb46e935488618ad1f
ItsArcal139 <zapto139@gmail.com>
2020-12-29 02:42:45 +0800

    small changes

--- Netherite/Worlds/World.cs
+++ Netherite/Worlds/World.cs
@@ -1,5 +1,8 @@
 ﻿using Netherite.Api.Worlds;
+using Netherite.Blocks;
 using Netherite.Data.Entities;
+using Netherite.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,5 +16,11 @@ namespace Netherite.Worlds
         public long Seed { get; set; }
 
         public Chunk GetChunk(int x, int z) => new Chunk(this, x, z);
+
+        public Block GetBlock(int x, int y, int z) => GetChunk(x, z).GetBlock(x % 16, y, z % 16);
+
+        public void SetBlock(int x, int y, int z, Block b) => GetChunk(x, z).SetBlock(x % 16, y, z % 16, b);
+
+        public Entity SpawnEntity<T>() where T : Entity => throw new NotImplementedException();
     }
 }
