db4b38550751e346450c0217f02d6254619aedbe
ItsArcal139 <zapto139@gmail.com>
2020-12-30 18:09:27 +0800

    Update to BlockState

---
+++ Netherite/Blocks/BlockState.cs
@@ -0,0 +1,30 @@
+﻿using Netherite.Data.Entities;
+using Netherite.Nbt;
+using System.Collections.Generic;
+
+namespace Netherite.Blocks
+{
+    public struct BlockState
+    {
+        public Identifier Id { get; set; }
+
+        public Dictionary<string, string> Properties { get; set; }
+
+        public BlockState(Identifier id)
+        {
+            Id = id;
+            Properties = new Dictionary<string, string>();
+        }
+
+        public BlockState(Identifier id, NbtCompound props)
+        {
+            Id = id;
+            Properties = new Dictionary<string, string>();
+
+            foreach (var p in props)
+            {
+                Properties.Add(p.Key, p.Value.ToValue());
+            }
+        }
+    }
+}
